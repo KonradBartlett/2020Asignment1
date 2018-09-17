5ff4c68b9786b4e7b98d51fbb5f4a47759f75a94
Konrad <konrad@lowfoot.com>
2018-09-17 13:01:19 -0400

    Term class

--- Assignment1/Assignment1/Term.cs
+++ Assignment1/Assignment1/Term.cs
@@ -14,13 +14,20 @@ namespace Assignment1
         // Creates a term with the given coefficient and exponent
         public Term (double coefficient, byte exponent)
         {
-
+            this.coefficient = coefficient;
+            this.exponent = exponent;
         }
 
         // Evaluates the current term for a given x
         public double Evaluate (double x)
         {
             double result = 0;
+
+            // for value of exponent, multiply coefficient by itself
+            for(int i = 0; i < exponent; i++)
+            {
+                result = coefficient * coefficient;
+            }
 
             return result;
         }
@@ -30,11 +37,52 @@ namespace Assignment1
         public int CompareTo (Object obj)
         {
             int result = 0;
+            Term term = obj as Term;
+
+            if (term.getExponent() < this.exponent)
+            {
+                result = -1;
+            }
+
+            if (term.getExponent() == this.exponent)
+            {
+                result = 0;
+            }
+
+            if (term.getExponent() > this.exponent)
+            {
+                result = 1;
+            }
 
             return result;
         }
 
         // Read and write properties for each data member
+        public double getCoefficient()
+        {
+            return this.coefficient;
+        }
 
+        public void setCoefficient(double coefficient)
+        {
+            this.coefficient = coefficient;
+        }
+
+        public byte getExponent()
+        {
+            return this.exponent;
+        }
+
+        public void setExponent(byte exponent)
+        {
+            this.exponent = exponent;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var term = obj as Term;
+            return term != null &&
+                   exponent == term.exponent;
+        }
     }
 }
